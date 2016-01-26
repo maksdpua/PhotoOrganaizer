c8754e656a75b88eef4808815d5f9c6fc03212ec
Maks.Shvec <maksdpua@gmail.com>
2016-01-26 18:51:54 +0200

    UIButton+PathString

---
+++ PhotoOrganaizer/Category/UIButton+StringPath/UIButton+PathString.m
@@ -0,0 +1,13 @@
+//
+//  UIButton+PathString.m
+//  PhotoOrganaizer
+//
+//  Created by Maks on 1/26/16.
+//  Copyright © 2016 Maks. All rights reserved.
+//
+
+#import "UIButton+PathString.h"
+
+@implementation UIButton (PathString)
+
+@end
