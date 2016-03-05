fd9b32bfb5093a9a31ce833ce15468910fac1f56
Maks <maksdpua@gmail.com>
2016-03-05 22:58:54 +0200

    MSCache

---
+++ PhotoOrganaizer/Classes/MSCache/MSCache.h
@@ -0,0 +1,13 @@
+//
+//  MSCache.h
+//  PhotoOrganaizer
+//
+//  Created by Maks on 3/5/16.
+//  Copyright © 2016 Maks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MSCache : NSObject
+
+@end
---
+++ PhotoOrganaizer/Classes/MSCache/MSCache.m
@@ -0,0 +1,13 @@
+//
+//  MSCache.m
+//  PhotoOrganaizer
+//
+//  Created by Maks on 3/5/16.
+//  Copyright © 2016 Maks. All rights reserved.
+//
+
+#import "MSCache.h"
+
+@implementation MSCache
+
+@end
